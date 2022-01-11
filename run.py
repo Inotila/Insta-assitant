f59cc37a84142d77f2477f16065a8849cee7bd21
Inotila <inoati1991@gmail.com>
2022-01-11 17:28:14 +0000

    update social media industry list

--- run.py
+++ run.py
@@ -4,13 +4,12 @@ print("Where we help your business reach more followers and create a buget for y
 
 name = input("Please enter your name:")
 
-social_media_acc_types = ["Retial","Artists","Music Band"]
+social_media_industry = ["Healthcare","Media","Education","Nonprofit","Restaurants","Tech", "Travel & Tourism", "Finance", "Hospitality"]
 
 number_of_followers_targeted = [1,2,3,4,5]
 
 def cost_to_reach_followers():
     users_target = input("Please enter the number of users you would like to reach:")
-    print(f"You would like to reach this many followers {users_target}")
 
 cost_to_reach_followers()
 
