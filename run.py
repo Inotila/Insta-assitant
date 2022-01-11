2319f81569a3ea357b850466b909138af1f74ba3
Inotila <inoati1991@gmail.com>
2022-01-11 18:40:54 +0000

    add a function to place the user in an industry

--- run.py
+++ run.py
@@ -10,7 +10,33 @@ print("Instgram industry \n 1.Healthcare \n 2.Media \n 3.Education \n 4.Nonprofi
 
 social_media_industry = input("\n Please enter which  number represents your industry:")
 
+def industry_indetifier():
+    global user_industry
+    user_industry = ""
 
-
+    if int(social_media_industry) == 1:
+        user_industry = "Healthcare"
+    elif int(social_media_industry) == 2:
+        user_industry = "Media"
+    elif int(social_media_industry) == 3:
+        user_industry = "Education"
+    elif int(social_media_industry) == 4:
+        user_industry = "Nonprofit"
+    elif int(social_media_industry) == 5:
+        user_industry = "Restaurants"
+    elif int(social_media_industry) == 6:
+        user_industry = "Tech"
+    elif int(social_media_industry) == 7:
+        user_industry = "Travel & Tourism"
+    elif int(social_media_industry) == 8:
+        user_industry = "Finance"
+    elif int(social_media_industry) == 9:
+        user_industry = "Hospitality"
+    else:
+        print("stupid")
     
+    return user_industry
 
+industry_indetifier()
+
+print(f"\nYou are in the {user_industry}")
