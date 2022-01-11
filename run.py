e418b3372c784c71072a78e4bbff9db41059e25f
Inotila <inoati1991@gmail.com>
2022-01-11 15:13:59 +0000

    add social media type list and a name input

--- run.py
+++ run.py
@@ -2,3 +2,9 @@ print("Welcome to Insta-Assitant\n")
 
 print("Where we help your business reach more followers and create a buget for you.\n")
 
+name = input("Please enter your name:")
+
+social_media_acc_types = ["Retial","Artists","Music Band"]
+print(social_media_acc_types)
+
+print(f"Hello {name}")
