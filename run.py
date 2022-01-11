c1ba6a0e5a7c3031c2227c6570be43e1164e51c3
Inotila <inoati1991@gmail.com>
2022-01-11 15:32:11 +0000

    add a function that take inpute of how many followers are desired

--- run.py
+++ run.py
@@ -8,4 +8,13 @@ social_media_acc_types = ["Retial","Artists","Music Band"]
 
 number_of_followers_targeted = [1,2,3,4,5]
 
+def cost_to_reach_followers():
+    users_target = input("Please enter the number of users you would like to reach:")
+    print(f"You would like to reach this many followers {users_target}")
+
+cost_to_reach_followers()
+
 print(f"Hello {name}")
+
+    
+
