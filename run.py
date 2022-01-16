85ed84a078b650e1469291e95a44fcf6842f936f
Inotila <inoati1991@gmail.com>
2022-01-16 22:06:03 +0000

    update text

--- run.py
+++ run.py
@@ -73,7 +73,7 @@ hospitality = {
 
 print(f"\n Hello {name}")
 
-print("Instgram industry \n 1.Healthcare \n 2.Media\
+print("\n Instagram industry options:\n 1.Healthcare \n 2.Media\
     \n 3.Education \n 4.Nonprofit \n 5.Restaurants \n 6.Tech\
     \n 7.Travel & Tourism \n 8.Finance \n 9.Hospitality ")
 
